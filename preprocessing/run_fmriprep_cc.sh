8e7a5ac0763312c7928cc5686e67f6906f0c94a9
Ludoalevesque <ludo.a.levesque@gmail.com>
2024-06-13 11:50:35 -0400

    still testing

--- preprocessing/run_fmriprep_cc.sh
+++ preprocessing/run_fmriprep_cc.sh
@@ -15,7 +15,7 @@
                                # subjects on 32 cpus-per-task), you could ask for one more node.
 #SBATCH --cpus-per-task=32     # --> You can see here the choices. For beluga, you can choose 32, 40 or 64.
                                # https://docs.computecanada.ca/wiki/B%C3%A9luga/en#Node_Characteristics
-#SBATCH --mem=70               # --> 0 means you take all the memory of the node. If you think you will need
+#SBATCH --mem=70G               # --> 0 means you take all the memory of the node. If you think you will need
                                # all the node, you can put 0.
 
 
